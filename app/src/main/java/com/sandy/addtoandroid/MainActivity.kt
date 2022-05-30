d8bd3c5efcdec903efd3888d0add5eba6c4f3d0c
sanidhya <sanidhya09@gmail.com>
2022-05-30 16:21:14 +0530

    Initial commit

---
+++ app/src/main/java/com/sandy/addtoandroid/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.sandy.addtoandroid
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+
+class MainActivity : AppCompatActivity() {
+    var flutterEngine: FlutterEngine? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+        // Configure an initial route.
+        flutterEngine!!.navigationChannel.setInitialRoute("your/route/here");
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine!!.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+    }
+
+    fun openFlutterModule(view: View) {
+        startActivity(
+            FlutterActivity
+                .withCachedEngine("my_engine_id")
+                .build(this)
+        )
+    }
+}
