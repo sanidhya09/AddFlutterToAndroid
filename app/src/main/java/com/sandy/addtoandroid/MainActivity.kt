8ccc260d63b1cdb5027ce38a82f6ee0ba85ebf6c
sanidhya <sanidhya09@gmail.com>
2022-11-08 14:42:13 +0530

    Added Book Data Model

--- app/src/main/java/com/sandy/addtoandroid/MainActivity.kt
+++ app/src/main/java/com/sandy/addtoandroid/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.sandy.addtoandroid
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.engine.FlutterEngineCache
-import io.flutter.embedding.engine.dart.DartExecutor
 
 
 class MainActivity : AppCompatActivity() {
-    var flutterEngine: FlutterEngine? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Move this initialization to Application Class
-        // Instantiate a FlutterEngine.
-        flutterEngine = FlutterEngine(this)
-        // Configure an initial route.
-        flutterEngine!!.navigationChannel.setInitialRoute("your/route/here");
 
-        // Start executing Dart code to pre-warm the FlutterEngine.
-        flutterEngine!!.dartExecutor.executeDartEntrypoint(
-            DartExecutor.DartEntrypoint.createDefault()
-        )
-
-        // Cache the FlutterEngine to be used by FlutterActivity.
-        FlutterEngineCache
-            .getInstance()
-            .put("my_engine_id", flutterEngine)
     }
 
     fun openFlutterModule(view: View) {
-        startActivity(
-            FlutterActivity
-                .withCachedEngine("my_engine_id")
-                .build(this)
+        val books = mutableListOf<Api.Book>()
+        val book = Api.Book()
+        book.title = "Sandy"
+        book.subtitle = "Kumar"
+        val thumbnail = Api.Thumbnail()
+        thumbnail.url = "http"
+        book.thumbnail = thumbnail
+        books.add(book)
+
+        startActivityForResult(
+            FlutterBookActivity
+                .withBook(this, books[0]), 0
         )
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        flutterEngine!!.destroy()
+    // Callback when the Flutter activity started with 'startActivityForResult' above returns.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // The Flutter activity may cancel the edit. If so, don't update anything.
+        if (resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                throw RuntimeException("The FlutterBookActivity returning RESULT_OK should always have a return data intent")
+            }
+            val returnedBook = FlutterBookActivity.getBookFromResultIntent(data)
+
+        }
     }
+
+
 }
